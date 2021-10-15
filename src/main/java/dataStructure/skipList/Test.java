ece03f8aa87c032f1e1178fd58bf560ae383a9a0
gaojw01 <gaojw01@mingyuanyun.com>
2021-10-15 17:07:59 +0800

    ok

---
+++ src/main/java/dataStructure/skipList/Test.java
@@ -0,0 +1,10 @@
+package dataStructure.skipList;
+
+import java.util.concurrent.ConcurrentSkipListSet;
+
+public class Test {
+    public static void main(String[] args) {
+        ConcurrentSkipListSet concurrentSkipListSet = new ConcurrentSkipListSet();
+
+    }
+}
