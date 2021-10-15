2c18d2ee3607424a2dc4a5a0d74ddafdaf5e4252
gaojw01 <gaojw01@mingyuanyun.com>
2021-10-15 11:20:24 +0800

    ok

---
+++ src/main/java/dataStructure/chartUnionFind/Test.java
@@ -0,0 +1,21 @@
+package dataStructure.chartUnionFind;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+
+        UnionFind uf = new UnionFind(10);
+        // 1-2-5-6-7 3-8-9 4
+        uf.union(1, 2);
+        uf.union(2, 5);
+        uf.union(5, 6);
+        uf.union(6, 7);
+        uf.union(3, 8);
+        uf.union(8, 9);
+        System.out.println(uf.connected(1, 5)); // true
+        System.out.println(uf.connected(5, 7)); // true
+        System.out.println(uf.connected(4, 9)); // false
+        // 1-2-5-6-7 3-8-9-4
+        uf.union(9, 4);
+        System.out.println(uf.connected(4, 9)); // true
+    }
+}
---
+++ src/main/java/dataStructure/chartUnionFind/UnionFind.java
@@ -0,0 +1,32 @@
+package dataStructure.chartUnionFind;
+
+public class UnionFind {
+    int root[];
+
+    public UnionFind(int size) {
+        root = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+        }
+    }
+
+    public int find(int x) {
+        return root[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            for (int i = 0; i < root.length; i++) {
+                if (root[i] == rootY) {
+                    root[i] = rootX;
+                }
+            }
+        }
+    };
+
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+}
